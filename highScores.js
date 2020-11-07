26d7fa2c5073893e2ee529a07e2f6a3448894b39
Paul Carr <pwcarr15@gmail.com>
2020-11-06 22:36:04 -0500

    Added section for adding highscores to local storage.

--- highScores.js
+++ highScores.js
@@ -4,3 +4,11 @@ clear.addEventListener("click", function() {
     location.reload();
 }); 
 
+var scores = localStorage.getItem("scores");
+scores = JSON.parse(scores);
+var highScore = document.querySelector("#highScore");
+for (var i = 0; i < scores.length; i++) {
+    var createLi = document.createElement("li");
+    createLi.textContent = scores[i].initials + " " + scores[i].score;
+    highScore.appendChild(createLi);
+}
