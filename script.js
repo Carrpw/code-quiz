d7eba7095379bc29b49ee20c13a1e8b98b11ec41
Paul Carr <pwcarr15@gmail.com>
2020-11-06 22:24:34 -0500

    Started a script with variables.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var score = 0
+//Variables
+var questionIndex = 0;
+var secondsLeft = 116;
+var pause = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#timer");
+var questions = document.querySelector("#questions");
+var wrapper = document.querySelector("#wrapper");
+var ul = document.createElement("ul");
+
+
